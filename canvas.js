913c8df0125fa786beda278e65a26466c9e48246
Harry Barr <harrybarr530@gmail.com>
2024-11-04 11:17:25 +0000

    nearly-done

---
+++ canvas.js
@@ -0,0 +1,176 @@
+const canvas = document.querySelector("#about-canvas");
+const c = canvas.getContext("2d");
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+const colors = ["rgba(128, 0, 128, 0.500)"];
+
+// Event Listeners
+addEventListener("mousemove", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+// HELPER FUNCTIONS
+
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function distance(x1, y1, x2, y2) {
+  const xDist = x2 - x1;
+  const yDist = y2 - y1;
+
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+}
+function rotate(velocity, angle) {
+  const rotatedVelocities = {
+    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle),
+  };
+
+  return rotatedVelocities;
+}
+function resolveCollision(particle, otherParticle) {
+  const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+  const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+
+  const xDist = otherParticle.x - particle.x;
+  const yDist = otherParticle.y - particle.y;
+
+  // Prevent accidental overlap of particles
+  if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+    // Grab angle between the two colliding particles
+    const angle = -Math.atan2(
+      otherParticle.y - particle.y,
+      otherParticle.x - particle.x
+    );
+
+    // Store mass in var for better readability in collision equation
+    const m1 = particle.mass;
+    const m2 = otherParticle.mass;
+
+    // Velocity before equation
+    const u1 = rotate(particle.velocity, angle);
+    const u2 = rotate(otherParticle.velocity, angle);
+
+    // Velocity after 1d collision equation
+    const v1 = {
+      x: (u1.x * (m1 - m2)) / (m1 + m2) + (u2.x * 2 * m2) / (m1 + m2),
+      y: u1.y,
+    };
+    const v2 = {
+      x: (u2.x * (m1 - m2)) / (m1 + m2) + (u1.x * 2 * m2) / (m1 + m2),
+      y: u2.y,
+    };
+
+    // Final velocity after rotating axis back to original location
+    const vFinal1 = rotate(v1, -angle);
+    const vFinal2 = rotate(v2, -angle);
+
+    // Swap particle velocities for realistic bounce effect
+    particle.velocity.x = vFinal1.x;
+    particle.velocity.y = vFinal1.y;
+
+    otherParticle.velocity.x = vFinal2.x;
+    otherParticle.velocity.y = vFinal2.y;
+  }
+}
+
+// Objects
+class Particle {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.velocity = {
+      x: Math.random() - 0.5 * 3,
+      y: Math.random() - 0.5 * 3,
+    };
+    this.radius = radius;
+    this.color = color;
+    this.mass = 1;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+
+    c.closePath();
+  }
+
+  update(particles) {
+    this.draw();
+    for (let i = 0; i < particles.length; i++) {
+      if (this === particles[i]) continue;
+      if (
+        distance(this.x, this.y, particles[i].x, particles[i].y) -
+          this.radius * 2 <
+        0
+      ) {
+        resolveCollision(this, particles[i]);
+      }
+    }
+    if (this.x - this.radius <= 0 || this.x + this.radius >= canvas.width) {
+      this.velocity.x = -this.velocity.x;
+    }
+    if (this.y - this.radius <= 0 || this.y + this.radius >= canvas.height) {
+      this.velocity.y = -this.velocity.y;
+    }
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+}
+
+// Implementation
+let particles;
+function init() {
+  particles = [];
+
+  for (let i = 0; i < 15; i++) {
+    const radius = Math.random() * 45 + 25;
+    let x = randomIntFromRange(radius, canvas.width - radius);
+    let y = randomIntFromRange(radius, canvas.height - radius);
+    const color = randomColor(colors);
+
+    if (i !== 0) {
+      for (let j = 0; j < particles.length; j++) {
+        if (distance(x, y, particles[j].x, particles[j].y) - radius * 2 < 0) {
+          x = randomIntFromRange(radius, canvas.width - radius);
+          y = randomIntFromRange(radius, canvas.height - radius);
+          j = -1;
+        }
+      }
+    }
+
+    particles.push(new Particle(x, y, radius, color));
+  }
+}
+
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update(particles);
+  }
+  // objects.forEach(object => {
+  //  object.update()
+  // })
+}
+
+window.onload = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight; // Adjust the height based on your needs
+
+  init();
+  animate();
+};
---
+++ script.js
@@ -0,0 +1,74 @@
+const aboutSection = document.querySelector("#about-section");
+const portfolioSection = document.querySelector("#portfolio-section");
+const skillsSection = document.querySelector("#skills-section");
+const contactSection = document.querySelector("#contact-section");
+const navBtns = document.querySelectorAll(".nav-btn");
+const projects = document.querySelectorAll(".project");
+const projectDescDiv = document.querySelector(".project-desc");
+const selectElement = document.querySelector("select");
+const gallery = document.querySelector(".gallery");
+
+navBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const selector = document.querySelector(`#${e.currentTarget.id}`);
+    if (selector.id === "about") {
+      aboutSection.scrollIntoView({
+        behavior: "smooth",
+      });
+    } else if (selector.id === "portfolio") {
+      portfolioSection.scrollIntoView({
+        behavior: "smooth",
+      });
+    } else if (selector.id === "skills") {
+      skillsSection.scrollIntoView({
+        behavior: "smooth",
+      });
+    } else {
+      contactSection.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+const projectArray = [];
+projects.forEach((project) => {
+  projectArray.push(project);
+});
+
+const originalGalleryContent = gallery.innerHTML;
+
+const filterProjects = (elementTag) => {
+  if (selectElement.value === "") {
+    gallery.innerHTML = originalGalleryContent;
+    return;
+  } else if (selectElement.value === elementTag) {
+    gallery.innerHTML = ""; // Clear the gallery
+    gallery.insertAdjacentHTML("beforeend", "<div class='gallery-div'></div>"); // Add a new div container
+
+    const galleryDiv = document.querySelector(".gallery-div");
+    const reactArr = projectArray.filter(
+      (project) => project.dataset.tag == elementTag
+    );
+
+    // Map to get HTML of each filtered element
+    const projectHTML = reactArr.map((project) => project.outerHTML).join("");
+
+    // Insert the HTML of filtered projects into galleryDiv
+    galleryDiv.insertAdjacentHTML("beforeend", projectHTML);
+  }
+};
+
+selectElement.addEventListener("change", () =>
+  filterProjects(selectElement.value)
+);
+projects.forEach((project) => {
+  project.addEventListener("mouseover", (e) => {
+    projectDescDiv.innerHTML = "";
+    const projectName = e.currentTarget.id;
+    projectDescDiv.insertAdjacentHTML("beforeend", `<h3>${projectName}</h3>`);
+  });
+  project.addEventListener("mouseout", (e) => {
+    projectDescDiv.innerHTML = ""; // Clear the content or set it back to default
+  });
+});
